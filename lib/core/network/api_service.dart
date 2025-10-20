70da502df2b5a51d59c4466a14e639857eecdfd8
ibrahim mohammed <ibrahom0780@gmail.com>
2025-10-20 10:14:40 +0300

    api service done 2

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -1,25 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:hungry_app/core/network/api_exception.dart';
 import 'package:hungry_app/core/network/dio_client.dart';
 
 class ApiService {
   DioClient _dioClient = DioClient();
 
   Future<dynamic> get(String endPoint) async {
-    final res = await _dioClient.dio.get(endPoint);
-    return res.data;
+    try {
+      final res = await _dioClient.dio.get(endPoint);
+      return res.data;
+    } on DioException catch (e) {
+      ApiException.errorHandler(e);
+    }
   }
 
   Future<dynamic> post(String endPoint, Map<String, dynamic> body) async {
-    final res = await _dioClient.dio.post(endPoint, data: body);
-    return res.data;
+    try {
+      final res = await _dioClient.dio.post(endPoint, data: body);
+      return res.data;
+    } on DioException catch (e) {
+      ApiException.errorHandler(e);
+    }
   }
 
   Future<dynamic> put(String endPoint, Map<String, dynamic> body) async {
-    final res = await _dioClient.dio.put(endPoint, data: body);
-    return res.data;
+    try {
+      final res = await _dioClient.dio.put(endPoint, data: body);
+      return res.data;
+    } on DioException catch (e) {
+      ApiException.errorHandler(e);
+    }
   }
 
   Future<dynamic> delete(String endPoint, Map<String, dynamic> body) async {
-    final res = await _dioClient.dio.delete(endPoint, data: body);
-    return res.data;
+    try {
+      final res = await _dioClient.dio.delete(endPoint, data: body);
+      return res.data;
+    } on DioException catch (e) {
+      ApiException.errorHandler(e);
+    }
   }
 }
