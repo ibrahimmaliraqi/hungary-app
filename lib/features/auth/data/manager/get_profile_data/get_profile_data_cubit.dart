6efade3a7030ba6dc5b71289eae49ff774b5dbe6
ibrahim mohammed <ibrahom0780@gmail.com>
2025-10-22 16:57:57 +0300

    get profile data cubit done

--- lib/features/auth/data/manager/get_profile_data/get_profile_data_cubit.dart
+++ lib/features/auth/data/manager/get_profile_data/get_profile_data_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:hungry_app/features/auth/data/models/user_model.dart';
+import 'package:hungry_app/features/auth/data/repos/auth_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'get_profile_data_state.dart';
 
 class GetProfileDataCubit extends Cubit<GetProfileDataState> {
-  GetProfileDataCubit() : super(GetProfileDataInitial());
+  GetProfileDataCubit(this.authRepo) : super(GetProfileDataInitial());
+  AuthRepo authRepo;
+  getProfileData() async {
+    emit(GetProfileDataLoading());
+    final result = await authRepo.getProfileData();
+    result.fold(
+      (fail) {
+        emit(GetProfileDataFailure(fail.errMessage));
+      },
+      (user) {
+        emit(GetProfileDataSuccess(user));
+      },
+    );
+  }
 }
--- lib/features/auth/data/manager/get_profile_data/get_profile_data_state.dart
+++ lib/features/auth/data/manager/get_profile_data/get_profile_data_state.dart
@@ -4,3 +4,17 @@ part of 'get_profile_data_cubit.dart';
 sealed class GetProfileDataState {}
 
 final class GetProfileDataInitial extends GetProfileDataState {}
+
+final class GetProfileDataLoading extends GetProfileDataState {}
+
+final class GetProfileDataSuccess extends GetProfileDataState {
+  final UserModel user;
+
+  GetProfileDataSuccess(this.user);
+}
+
+final class GetProfileDataFailure extends GetProfileDataState {
+  final String errMessage;
+
+  GetProfileDataFailure(this.errMessage);
+}
